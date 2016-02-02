1de9c6a32521b3e58eb8c4d70bcfdd12540ec37b
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-02-02 15:04:02 -0200

    Primeiro commit: adicionados arquivos para criação dos usuários alunos e ativação do freeze.

---
+++ criar-usuarios-alunos.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+addgroup freeze
+adduser --gecos "Modelo para Freeze" --shell /bin/bash freezetemplate
+
+for i in 1 2 3
+do
+    adduser --disabled-login --gecos "Aluno #${i}" --groups freeze --shell /bin/bash aluno${i}
+    echo aluno${i}:aluno${i} | chpasswd
+done
+
+apt update
+apt install libpam-mount
+
+for s in login lightdm lightdm-autologin
+do
+    pam-config --service ${s} -a --mount
+done
+
+install -m 755 mount-wrapper /usr/local/sbin
+install -m 644 pam_mount.conf.xml /etc/security
