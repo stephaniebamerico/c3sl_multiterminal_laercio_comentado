5d08776a1bc679635efa8b758e6697a468574a2e
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-02-03 12:15:38 -0200

    Instalar e configurar automaticamente o compton para melhorar a aceleração da tela na placa TN-502.

--- configurar-multiterminal.sh
+++ configurar-multiterminal.sh
@@ -9,13 +9,15 @@ install -m 644 xorg/* /etc/X11/xorg.conf.d
 install -d /etc/lightdm/lightdm.conf.d
 install -m 644 lightdm/* /etc/lightdm/lightdm.conf.d
 
+install -m 644 autostart/compton.desktop /etc/xdg/autostart
+
 systemctl enable xorg@90.socket
 systemctl start xorg@90.socket
 
 apt-add-repository ppa:ubuntu-multiseat/ppa
 apt update
 apt -y upgrade
-apt -y install xserver-xorg-video-siliconmotion xserver-xephyr
+apt -y install xserver-xorg-video-siliconmotion xserver-xephyr compton
 
 udevadm trigger
 systemctl restart lightdm
