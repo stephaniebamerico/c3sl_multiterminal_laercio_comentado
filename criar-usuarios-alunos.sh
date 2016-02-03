1141b887d41695d32bef32f4db091dad458c210a
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-02-03 12:10:39 -0200

    Correções no script para criar os usuários alunos.

--- criar-usuarios-alunos.sh
+++ criar-usuarios-alunos.sh
@@ -5,12 +5,13 @@ adduser --gecos "Modelo para Freeze" --shell /bin/bash freezetemplate
 
 for i in 1 2 3
 do
-    adduser --disabled-login --gecos "Aluno #${i}" --groups freeze --shell /bin/bash aluno${i}
+    adduser --disabled-login --gecos "Aluno #${i}" --shell /bin/bash aluno${i}
+    adduser aluno${i} freeze
     echo aluno${i}:aluno${i} | chpasswd
 done
 
 apt update
-apt install libpam-mount
+apt -y install libpam-mount
 
 install -m 755 mount-wrapper /usr/local/sbin
 install -m 644 pam_mount.conf.xml /etc/security
