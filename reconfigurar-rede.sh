679a62519ae2b031a8d2925a356863d4003a14ae
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-02-04 13:35:16 -0200

    Adicionado script para reconfigurar a rede usando o systemd-metworkd (mais leve que o Network Manager).

---
+++ reconfigurar-rede.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+install -m 644 systemd/*.network /etc/systemd/network
+
+systemctl stop NetworkManager
+systemctl disable NetworkManager
+
+systemctl enable systemd-networkd
+systemctl enable systemd-networkd-wait-online
+systemctl enable systemd-resolved
+
+systemctl start systemd-networkd
+systemctl start systemd-networkd-wait-online
+systemctl start systemd-resolved
+
+ls -sf /var/run/systemd/resolve/resolv.conf /etc/resolv.conf
