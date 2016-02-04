28eb66ee2027082e4291996d3cfa6a542fff3bbe
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-02-04 12:19:16 -0200

    Os arquivos de configuração do LightDM agora são instalados no diretório /etc/xdg/lightdm.

--- configurar-multiterminal.sh
+++ configurar-multiterminal.sh
@@ -6,9 +6,8 @@ install -m 644 udev/* /etc/udev/rules.d
 install -d /etc/X11/xorg.conf.d
 install -m 644 xorg/* /etc/X11/xorg.conf.d
 
-install -d /etc/lightdm/lightdm.conf.d
-install -m 644 lightdm/* /etc/lightdm/lightdm.conf.d
-
+install -d /etc/xdg/lightdm/lightdm.conf.d
+install -m 644 lightdm/xephyr* /etc/xdg/lightdm/lightdm.conf.d
 install -m 644 autostart/compton.desktop /etc/xdg/autostart
 install -m 755 xephyr-wrapper /usr/local/bin
 
