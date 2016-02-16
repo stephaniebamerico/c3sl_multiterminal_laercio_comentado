ca6073a20ca856d10e6c396e835b4a5c9103703c
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-02-16 07:33:52 -0200

    Adicionado arquivo modelo para configuração do login automático.

--- configurar-multiterminal.sh
+++ configurar-multiterminal.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-install systemd/xorg@.s* /etc/systemd/system
+install -m 644 systemd/xorg@.s* /etc/systemd/system
 install -m 644 udev/* /etc/udev/rules.d
 
 install -d /etc/X11/xorg.conf.d
 install -m 644 xorg/* /etc/X11/xorg.conf.d
 
 install -d /etc/xdg/lightdm/lightdm.conf.d
-install -m 644 lightdm/xephyr* /etc/xdg/lightdm/lightdm.conf.d
+install -m 644 lightdm/xephyr*.conf /etc/xdg/lightdm/lightdm.conf.d
+install -m 644 lightdm/autologin.conf /etc/xdg/lightdm/lightdm.conf.d
 install -m 755 xephyr-wrapper /usr/local/bin
 
 systemctl enable xorg@90.service
