4b3a89f25acb368758e16ffc7334d355cba1d0db
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-02-16 16:05:14 -0200

    O script da tela listrada agora executa o comando update-grub automaticamente, se necessário.

--- contornar-bug-tela-listrada.sh
+++ contornar-bug-tela-listrada.sh
@@ -1,29 +1,46 @@
 #!/bin/bash
 
 indice=0
+particao=
 le5_encontrado=false
 
-while read -r linha
-do
-    if echo "${linha}" | grep "menuentry.*Linux Educacional 5.0" >/dev/null
-    then
-        particao=$(echo ${linha} | cut -d' ' -f7 | cut -d')' -f1)
-        le5_encontrado=true
-        break
-    else
-        indice=$(( indice + 1 ))
-    fi
-done < <(grep "^menuentry\|submenu" /boot/grub/grub.cfg)
+encontrar_le5() {
+    while read -r linha
+    do
+        if echo "${linha}" | grep "menuentry.*Linux Educacional 5.0" >/dev/null
+        then
+            particao=$(echo ${linha} | cut -d' ' -f7 | cut -d')' -f1)
+            le5_encontrado=true
+            break
+        else
+            indice=$(( indice + 1 ))
+        fi
+    done < <(grep "^menuentry\|submenu" /boot/grub/grub.cfg)
+}
+
+encontrar_le5
 
 if ! ${le5_encontrado}
 then
+    # Partição do LE 5.0 não detectada.
+    # Atualizando o menu do GRUB e tentando novamente...
+    indice=0
+    update-grub
+    encontrar_le5
+fi
+
+if ! ${le5_encontrado}
+then
+    echo
     echo "[ERRO] Partição do Linux Educacional 5.0 não detectada!"
     echo "       Certifique-se de que o Linux Educacional 5.0"
-    echo "       está instalado em uma outra partição e execute"
-    echo "       o comando 'update-grub' neste sistema"
-    echo "       para atualizar o menu do GRUB."
+    echo "       está instalado em uma outra partição."
     exit 1
 fi
+
+echo
+echo "[INFO] Partição do Linux Educacional 5.0 encontrada em ${partição}."
+echo "       Índice correspondente no menu do GRUB: ${indice}."
 
 install -m 644 systemd/le-nextboot-* /etc/systemd/system
 
@@ -39,7 +56,7 @@ umount /mnt
 
 echo
 echo "[AVISO] Agora você deve desligar o computador e ligá-lo novamente."
-echo "        O computador deve iniciar no Linux Educacional e reiniciar"
+echo "        O computador deve iniciar no Linux Educacional 5.0 e reiniciar"
 echo "        automaticamente de volta para este sistema."
 echo
 echo "        Uma vez concluído o processo, você pode reiniciar o computador"
