ae7527f0ec5837d1c692c1b49b21fb04936b9de0
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-02-22 09:52:22 -0300

    Novo script para configurar o multiterminal nos computadores do pregão 68/2009 (2º lote).

---
+++ configurar-multiterminal-rural.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+install -m 644 systemd/xorg@.s* /etc/systemd/system
+install -m 644 udev/71-* /etc/udev/rules.d
+
+install -d /etc/X11/xorg.conf.d
+install -m 644 xorg/99-tn502.conf.in /etc/X11/xorg.conf.d
+install -m 755 update-xorg-conf /usr/local/bin
+
+install -d /etc/xdg/lightdm/lightdm.conf.d
+install -m 644 lightdm/xephyr*.conf /etc/xdg/lightdm/lightdm.conf.d
+install -m 644 lightdm/autologin.conf /etc/xdg/lightdm/lightdm.conf.d
+
+install -m 755 xephyr-wrapper /usr/local/bin
+ln -s xephyr-wrapper /usr/local/bin/xephyr-wrapper-0
+ln -s xephyr-wrapper /usr/local/bin/xephyr-wrapper-1
+
+update-xorg-conf
+systemctl enable xorg@rural.socket
+systemctl start xorg@rural.socket
+
+apt-add-repository ppa:ubuntu-multiseat/ppa
+apt update
+apt -y upgrade
+apt -y install xserver-xorg-video-siliconmotion xserver-xephyr compton
+
+udevadm trigger
+systemctl restart lightdm
