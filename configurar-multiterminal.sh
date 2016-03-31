c691fc205927d18110f039f104174a02ea5ca2bc
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-03-31 14:07:30 -0300

    Corrigido o script configurar-multiterminal.sh

--- configurar-multiterminal.sh
+++ configurar-multiterminal.sh
@@ -4,7 +4,7 @@ install -m 644 systemd/xorg-daemon.s* /etc/systemd/system
 install -m 644 udev/* /etc/udev/rules.d
 
 install -d /etc/X11/xorg.conf.d
-install -m 644 xorg/99-tn502.conf.in /etc/X11/xorg.conf.d
+install -m 644 xorg/*.conf /etc/X11/xorg.conf.d
 install -m 755 update-xorg-conf /usr/local/bin
 install -m 755 xorg-daemon /usr/local/bin
 
