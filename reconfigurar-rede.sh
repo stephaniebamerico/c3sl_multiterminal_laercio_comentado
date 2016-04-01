4cc2aee232dd01c94542fcd3672b1d5e5365f23c
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-04-01 10:36:27 -0300

    Não ativa mais o wpa_supplicant ao executar o script reconfigurar-rede.sh. Isso evita problemas numa eventual clonagem do sistema para computadores com outros nomes de interface de rede wifi.

--- reconfigurar-rede.sh
+++ reconfigurar-rede.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-wifi_iface=$(ip -o link | grep wl | cut -d: -f2 | awk '{print $1}')
-
 install -m 644 systemd/cabeada.network /etc/systemd/network
 
 if [ -n "${wifi_iface}" ]
@@ -9,7 +7,7 @@ then
     install -m 644 systemd/wifi.network /etc/systemd/network
     install -m 644 systemd/wpa_supplicant@.service /etc/systemd/system
     install -d /etc/wpa_supplicant
-    install -m 644 systemd/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant-${wifi_iface}.conf
+    install -m 644 systemd/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf
 fi
 
 systemctl stop NetworkManager
@@ -25,11 +23,5 @@ systemctl enable systemd-resolved
 systemctl start systemd-networkd
 systemctl start systemd-networkd-wait-online
 systemctl start systemd-resolved
-
-if [ -n "${wifi_iface}" ]
-then
-    systemctl enable wpa_supplicant@${wifi_iface}
-    systemctl start wpa_supplicant@${wifi_iface}
-fi
 
 ln -sf /var/run/systemd/resolve/resolv.conf /etc/resolv.conf
