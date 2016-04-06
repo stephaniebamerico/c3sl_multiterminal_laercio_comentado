58b14b8074a5a6ee71d1d286723c759f5c90d500
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-04-06 16:51:19 -0300

    Removido o script xephyr-wrapper e simplificada a configuração do multiterminal, após constatar que a ativação por socket do Xorg está funcionando corretamente no UBuntu 16.04.

--- configurar-multiterminal.sh
+++ configurar-multiterminal.sh
@@ -11,10 +11,6 @@ install -m 755 xorg-daemon /usr/local/bin
 install -d /etc/xdg/lightdm/lightdm.conf.d
 install -m 644 lightdm/*.conf /etc/xdg/lightdm/lightdm.conf.d
 
-install -m 755 xephyr-wrapper /usr/local/bin
-ln -s xephyr-wrapper /usr/local/bin/xephyr-wrapper-0
-ln -s xephyr-wrapper /usr/local/bin/xephyr-wrapper-1
-
 update-xorg-conf
 systemctl enable xorg-daemon.socket
 systemctl start xorg-daemon.socket
