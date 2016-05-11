b64b6d475c46bfe4a9f7b6533b81dcec78f38829
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-05-11 13:47:47 -0300

    Adicionado script para automatizar a instalação de pacotes recomendados.

---
+++ instalar-pacotes-recomendados.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+PACOTES="
+ubuntu-edu-preschool
+ubuntu-edu-primary
+ubuntu-edu-secondary
+language-pack-kde-pt
+gparted
+pinta
+vlc
+ktouch
+openshot
+dreamchess
+gnome-chess
+supertux
+supertuxkart
+pingus
+chromium-browser
+adobe-flashplugin
+smc
+epoptes-client
+"
+
+OMNITUX="http://downloads.sourceforge.net/project/omnitux/omnitux/v1.2.1/omnitux_1.2.1_all.deb?r=http%3A%2F%2Fomnitux.sourceforge.net%2Fdownload.en.php&ts=1462984447&use_mirror=liquidtelecom"
+wget -c $OMNITUX -P /tmp/ && apt install /tmp/omnitux_1.2.1_all.deb
+
+apt install $PACOTES
