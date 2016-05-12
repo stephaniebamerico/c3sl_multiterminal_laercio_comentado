0fcd2ecb93438ab5be72fa2c13708f31bdf23de8
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-05-12 12:38:35 -0300

    Adiciona wine à lista de pacotes recomendados.

--- instalar-pacotes-recomendados.sh
+++ instalar-pacotes-recomendados.sh
@@ -21,6 +21,7 @@ ssvnc
 epoptes-client
 ssh
 nginx-light
+wine
 "
 
 OMNITUX="http://downloads.sourceforge.net/project/omnitux/omnitux/v1.2.1/omnitux_1.2.1_all.deb?r=http%3A%2F%2Fomnitux.sourceforge.net%2Fdownload.en.php&ts=1462984447&use_mirror=liquidtelecom"
