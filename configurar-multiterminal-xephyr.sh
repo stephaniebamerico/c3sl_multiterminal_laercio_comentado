aa45a21f1bdae7af5fd001908275a4465665c9a9
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2016-05-18 10:12:52 -0300

    Atualizadas as referências aos PPAs do projeto Ubuntu Multiseat.

--- configurar-multiterminal-xephyr.sh
+++ configurar-multiterminal-xephyr.sh
@@ -4,22 +4,23 @@ install -m 644 systemd/xorg-daemon.s* /etc/systemd/system
 install -m 644 udev/* /etc/udev/rules.d
 
 install -d /etc/X11/xorg.conf.d
-install -m 644 xorg/*.conf /etc/X11/xorg.conf.d
+install -m 644 xorg/9[78]*.conf /etc/X11/xorg.conf.d
 install -m 755 seat-attach-assistant /usr/local/bin
 install -m 755 update-xorg-conf /usr/local/bin
 install -m 755 xorg-daemon /usr/local/bin
+install -m 755 xephyr-wrapper /usr/local/bin
 
 install -d /etc/xdg/lightdm/lightdm.conf.d
-install -m 644 lightdm/9[5679]*.conf /etc/xdg/lightdm/lightdm.conf.d
+install -m 644 lightdm/*.conf /etc/xdg/lightdm/lightdm.conf.d
 
 update-xorg-conf SM501 /etc/X11/xorg.conf.d/98-proinfo-*.conf
 systemctl enable xorg-daemon.socket
 systemctl start xorg-daemon.socket
 
-apt-add-repository ppa:ubuntu-multiseat/xf86-video-nested
+apt-add-repository ppa:ubuntu-multiseat/xephyr
 apt update
 apt -y upgrade
-apt -y install xserver-xorg-video-siliconmotion xserver-xorg-video-nested compton numlockx
+apt -y install xserver-xorg-video-siliconmotion xserver-xephyr compton numlockx
 
 udevadm trigger
 systemctl restart lightdm
