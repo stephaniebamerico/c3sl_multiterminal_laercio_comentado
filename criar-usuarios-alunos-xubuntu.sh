32f84481edded2be73f044a616398017b50baa20
Laércio de Sousa <laerciosousa@sme-mogidascruzes.sp.gov.br>
2017-04-13 14:48:11 -0300

    Parametrizado o nome do usuário freezetemplate no script criar-usuarios-alunos-xubuntu.sh

--- criar-usuarios-alunos-xubuntu.sh
+++ criar-usuarios-alunos-xubuntu.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+freeze_template_user="alunot"
+freeze_template_fullname="Modelo para Aluno"
+
 addgroup freeze
-adduser --gecos "Modelo para Freeze" --shell /bin/bash freezetemplate
+adduser --gecos "${freeze_template_fullname}" --shell /bin/bash ${freeze_template_user}
+echo "${freeze_template_user}:aluno" | chpasswd
 
 for i in 0 1 2 3 4
 do
     adduser --disabled-login --gecos "Aluno #${i}" --shell /bin/bash aluno${i}
     adduser aluno${i} freeze
-    echo aluno${i}:aluno${i} | chpasswd
+    echo "aluno${i}:aluno" | chpasswd
 done
 
 apt update
@@ -21,12 +25,12 @@ install -m 644 pam_mount.conf.xml /etc/security
 install -m 644 xubuntu/*.policy /usr/share/polkit-1/actions
 
 install -m 755 freeze-session-auto /usr/local/bin
-install -d /home/freezetemplate/.config/autostart
-install -m 644 autostart/freeze-session-auto.desktop /home/freezetemplate/.config/autostart
-install -m 644 autostart-disable/*.desktop /home/freezetemplate/.config/autostart
+install -d /home/${freeze_template_user}/.config/autostart
+install -m 644 autostart/freeze-session-auto.desktop /home/${freeze_template_user}/.config/autostart
+install -m 644 autostart-disable/*.desktop /home/${freeze_template_user}/.config/autostart
 
-install -d /home/freezetemplate/.config/Thunar
-install -m 644 xubuntu/uca.xml /home/freezetemplate/.config/Thunar
-chown -R freezetemplate:freezetemplate /home/freezetemplate
+install -d /home/${freeze_template_user}/.config/Thunar
+install -m 644 xubuntu/uca.xml /home/${freeze_template_user}/.config/Thunar
+chown -R "${freeze_template_user}:${freeze_template_user}" /home/${freeze_template_user}
 
 mkdir -pm 0777 /var/freeze-data/{documents,pictures,music,videos}
